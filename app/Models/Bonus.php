ef0b2378298ac87f868d88dd8fe7b9cb3b859af5
YediRisdianto <yedi.risdianto1@gmail.com>
2025-07-31 21:14:26 +0700

    add : migration and relation model

---
+++ app/Models/Bonus.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bonus extends Model
+{
+    protected $fillable = [
+        'boarding_house_id',
+        'name',
+        'description',
+        'image',
+    ];
+
+    public function boardingHouse()
+    {
+        return $this->belongsTo(BoardingHouse::class);
+    }
+}
